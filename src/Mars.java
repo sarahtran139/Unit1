a9bb1455fac987084c9503ecbda0ef524a7c7d6a
Tran Diem Trinh Nguy <137216117+sarahtran139@users.noreply.github.com>
2023-08-21 14:54:03 -0400

    Add files via upload

--- src/Mars.java
+++ src/Mars.java
@@ -17,3 +17,21 @@ public class Mars {
         }
         landing = landingCheck(100);
     }
+
+    private static boolean landingCheck(int minutesLeft) throws InterruptedException {
+        for (int minute = 0; minute <= minutesLeft; i++) {
+            if (((minute % 3) == 0) && ((minute % 2) == 0)) {
+                System.out.println("Keep Center");
+            } else if ((minute % 2) == 0) {
+                System.out.println("Right");
+            } else if ((minute % 3) == 0) {
+                System.out.println("Left");
+            } else {
+                System.out.println("Calculating");
+            }
+            Thread.sleep(250);
+        }
+        System.out.println("Landed");
+        return false;
+    }
+}
